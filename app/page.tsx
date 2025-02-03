7626942116c4454fdf5b7e8f7a567a22aeff5443
Park JunYong <dyden119@gmail.com>
2025-02-03 17:40:54 +0900

    chore: 한글 폰트 추가

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <Container>
       <h1 className="text-[72px] font-yuji">単語道場</h1>
       <h1 className="text-[72px]">人類社会のすべての</h1>
+      <h1 className="text-[72px]">프리텐다드</h1>
     </Container>
   );
 }
