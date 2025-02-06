2d75939af32b0e8a780145d43b66111199aec163
Park JunYong <dyden119@gmail.com>
2025-02-06 18:47:55 +0900

    chore: 중복된 이메일 에러처리

--- app/api/sign-up/route.ts
+++ app/api/sign-up/route.ts
@@ -8,6 +8,10 @@ interface IFormData {
   password: string;
 }
 
+interface IMySqlError extends Error {
+  code?: string;
+}
+
 export async function POST(req: Request) {
   try {
     const data: IFormData = await req.json();
@@ -16,7 +20,7 @@ export async function POST(req: Request) {
     // 프론트단에서 막았지만 한번 더 체크
     if (!data.email || !data.name || !data.password) {
       return NextResponse.json(
-        { message: "입력 안한게 있잖아" },
+        { message: "양식에 공란 있음" },
         { status: 400 }
       );
     }
@@ -32,7 +36,13 @@ export async function POST(req: Request) {
 
     return NextResponse.json({ message: " 회원가입 성공" }, { status: 200 });
   } catch (e) {
-    console.log(e);
+    const error = e as IMySqlError;
+
+    if (error.code === "ER_DUP_ENTRY") {
+      console.log(error.code);
+      return NextResponse.json({ message: "중복된 이메일" }, { status: 400 });
+    }
+
     return NextResponse.json({ message: "서버 오류" }, { status: 500 });
   }
 }
