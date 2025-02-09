92e19289ed54a45e93f8b949fbf2e5cc6665bdec
Park JunYong <dyden119@gmail.com>
2025-02-09 14:21:53 +0900

    chore: 토큰이 있으면 로그인, 회원가입 페이지 접근 못하게

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("dojoAccessToken")?.value;
+  const { pathname } = req.nextUrl;
+
+  if (token && (pathname === "/" || pathname.startsWith("/sign-up"))) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
 
   if (!token) {
     return NextResponse.redirect(new URL("/", req.url));
@@ -11,5 +16,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: ["/dashboard/:path*", "/", "/sign-up"],
 };
