42fbd3d5d7388bb65ae413a5e337d0f4ef1256e5
Park JunYong <dyden119@gmail.com>
2025-02-09 15:25:00 +0900

    fix: 미들웨어 무한 리디렉팅 현상 픽스

--- middleware.ts
+++ middleware.ts
@@ -4,17 +4,25 @@ export function middleware(req: NextRequest) {
   const token = req.cookies.get("dojoAccessToken")?.value;
   const { pathname } = req.nextUrl;
 
-  if (token && (pathname === "/" || pathname.startsWith("/sign-up"))) {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
+  // 1. 토큰이 있는 경우
+  if (token) {
+    // 1-1. 루트 페이지 또는 회원가입 페이지에 접근 시 -> 대시보드로 리디렉션
+    if (pathname === "/" || pathname === "/sign-up") {
+      return NextResponse.redirect(new URL("/dashboard", req.url));
+    }
   }
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/", req.url));
+  // 2. 토큰이 없는 경우
+  else {
+    // 2-1. 대시보드 관련 페이지에 접근 시 -> 루트 페이지로 리디렉션
+    if (pathname.startsWith("/dashboard")) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/", "/sign-up"],
+  matcher: ["/dashboard/:path*", "/sign-up", "/"],
 };
