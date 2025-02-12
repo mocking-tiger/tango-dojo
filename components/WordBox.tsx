01eacc49d9aab330c9c1ebf12e386e889861a21b
Park JunYong <dyden119@gmail.com>
2025-02-12 22:19:54 +0900

    feat: 단어 클릭시 뜻, 예문등 출력

--- components/WordBox.tsx
+++ components/WordBox.tsx
@@ -6,20 +6,65 @@ import { useState } from "react";
 
 export default function WordBox({ words }: { words: typeof N5WORDS }) {
   const [index, setIndex] = useState(0);
+  const [isSelected, setIsSelected] = useState(false);
 
-  const TTS = () => {
-    const utterance = new SpeechSynthesisUtterance(words[index].kana);
+  const TTS = (word: string) => {
+    const utterance = new SpeechSynthesisUtterance(word);
     utterance.lang = "ja-JP";
     utterance.rate = 0.5;
     utterance.pitch = 1;
 
     speechSynthesis.speak(utterance);
   };
+
+  const handleNextWord = (direction: string) => {
+    setIsSelected(false);
+    setTimeout(() => {
+      if (direction === "next") {
+        setIndex((prev) => prev + 1);
+      } else {
+        setIndex((prev) => prev - 1);
+      }
+    }, 100);
+  };
+
   // console.log(words);
   return (
     <article className="w-[80%] h-[80%] bg-white rounded-[15px] relative">
       <div className="w-full h-full flex flex-col justify-center items-center">
-        <h1 className="text-[36px] md:text-[72px]">{words[index].word}</h1>
+        <h1
+          className="text-[36px] md:text-[72px] cursor-pointer"
+          onClick={() => setIsSelected((prev) => !prev)}
+        >
+          {words[index].word}
+        </h1>
+
+        <div
+          className={`px-[50px] md:px-0 flex flex-col justify-center items-center transition-all ease-in-out ${
+            isSelected ? "scale-100" : "scale-0"
+          }`}
+        >
+          <h2 className="text-[24px] md:text-[36px]">{words[index].kana}</h2>
+          <h2 className="mb-[60px] text-[24px] md:text-[36px]">
+            {words[index].mean}
+          </h2>
+          <div className="relative text-center">
+            <h2 className="text-[20px] md:text-[24px]">
+              {words[index].example}
+            </h2>
+            <h2 className="text-[20px] md:text-[24px]">
+              {words[index].exampleMean}
+            </h2>
+            <div className="w-[25px] md:w-[50px] h-[25px] md:h-[50px] absolute -right-[30px] md:-right-[100px] top-[20%] md:top-[15%] cursor-pointer">
+              <Image
+                src={"/speaker.png"}
+                fill
+                alt="next-arrow-icon"
+                onClick={() => TTS(words[index].example)}
+              />
+            </div>
+          </div>
+        </div>
       </div>
       {index !== 0 && (
         <Image
@@ -30,7 +75,7 @@ export default function WordBox({ words }: { words: typeof N5WORDS }) {
           sizes="100vw"
           style={{ width: "40px", height: "auto" }}
           alt="prev-arrow-icon"
-          onClick={() => setIndex((prev) => prev - 1)}
+          onClick={() => handleNextWord("prev")}
         />
       )}
       {index !== words.length - 1 && (
@@ -42,11 +87,16 @@ export default function WordBox({ words }: { words: typeof N5WORDS }) {
           sizes="100vw"
           style={{ width: "40px", height: "auto" }}
           alt="next-arrow-icon"
-          onClick={() => setIndex((prev) => prev + 1)}
+          onClick={() => handleNextWord("next")}
         />
       )}
-      <div className="w-[25px] md:w-[50px] h-[25px] md:h-[50px] absolute right-[30px] md:right-[30px] top-[48.5%] md:top-[47%] cursor-pointer">
-        <Image src={"/speaker.png"} fill alt="next-arrow-icon" onClick={TTS} />
+      <div className="w-[25px] md:w-[50px] h-[25px] md:h-[50px] absolute right-[30px] md:right-[30px] top-[27.5%] md:top-[30%] cursor-pointer">
+        <Image
+          src={"/speaker.png"}
+          fill
+          alt="next-arrow-icon"
+          onClick={() => TTS(words[index].kana)}
+        />
       </div>
       <aside className="text-[20px] font-yuji absolute -top-[50px] right-[40%] md:right-[47.5%]">{`${
         index + 1
