8de550967a19674b89c670c25dc0ad2950a88445
Park JunYong <dyden119@gmail.com>
2025-02-23 23:19:02 +0900

    chore: 데이터 잘 받아오는지 테스트

--- back-tango-dojo/index.ts
+++ back-tango-dojo/index.ts
@@ -14,3 +14,13 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`서버 돌아가는중 포트번호: ${PORT}`);
 });
+
+app.get("/users", async (req, res) => {
+  try {
+    const response = await prisma.user.findMany();
+    res.json(response);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "서버 오류" });
+  }
+});
