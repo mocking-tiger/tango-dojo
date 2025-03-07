4ea73c45a0ebd5d694a94a1639145983a58f5828
Park JunYong <dyden119@gmail.com>
2025-03-07 23:06:11 +0900

    chore: 단어삽입기 틀

--- front-tango-dojo/app/dev/words-inserter/page.tsx
+++ front-tango-dojo/app/dev/words-inserter/page.tsx
@@ -1,3 +1,118 @@
+"use client";
+
+import { useState } from "react";
+
+interface Word {
+  word: string;
+  kana: string;
+  mean: string;
+  example: string;
+  exampleKana: string;
+  exampleMean: string;
+}
+
+const initialState = {
+  word: "",
+  kana: "",
+  mean: "",
+  example: "",
+  exampleKana: "",
+  exampleMean: "",
+};
+
 export default function WordInserter() {
-  return <div>하위폴더에서 커밋하면 어떻게 되는지 테스트</div>;
+  const [newWord, setNewWord] = useState<Word>(initialState);
+  const [words, setWords] = useState<Word[]>([]);
+
+  const addWord = () => {
+    setWords((prev) => [...prev, newWord]);
+  };
+
+  return (
+    <div>
+      <div className="w-fit flex flex-col gap-3 border border-black">
+        <label>WORD</label>
+        <input
+          className="w-[600px] p-3 border border-black"
+          value={newWord?.word}
+          onChange={(e) =>
+            setNewWord((prev) => ({ ...prev, word: e.target.value }))
+          }
+        />
+        <label>KANA</label>
+        <input
+          className="w-[600px] p-3 border border-black"
+          value={newWord?.kana}
+          onChange={(e) =>
+            setNewWord((prev) => ({ ...prev, kana: e.target.value }))
+          }
+        />
+        <label>MEAN</label>
+        <input
+          className="w-[600px] p-3 border border-black"
+          value={newWord?.mean}
+          onChange={(e) =>
+            setNewWord((prev) => ({ ...prev, mean: e.target.value }))
+          }
+        />
+        <label>EXAMPLE</label>
+        <input
+          className="w-[600px] p-3 border border-black"
+          value={newWord?.example}
+          onChange={(e) =>
+            setNewWord((prev) => ({ ...prev, example: e.target.value }))
+          }
+        />
+        <label>EXAMPLE KANA</label>
+        <input
+          className="w-[600px] p-3 border border-black"
+          value={newWord?.exampleKana}
+          onChange={(e) =>
+            setNewWord((prev) => ({ ...prev, exampleKana: e.target.value }))
+          }
+        />
+        <label>EXAMPLE MEAN</label>
+        <input
+          className="w-[600px] p-3 border border-black"
+          value={newWord?.exampleMean}
+          onChange={(e) =>
+            setNewWord((prev) => ({ ...prev, exampleMean: e.target.value }))
+          }
+        />
+        <button
+          className="w-fit p-5 border border-black"
+          onClick={() => {
+            console.log("버튼 클릭");
+            addWord();
+          }}
+        >
+          리스트에 추가
+        </button>
+      </div>
+      <div>
+        <h1>삽입 리스트</h1>
+        <div className="flex flex-wrap gap-3 border border-black">
+          {words.map((word, index) => (
+            <ul
+              key={index}
+              className="w-fit flex flex-col gap-2 border border-black"
+            >
+              <li>{word.word}</li>
+              <li>{word.kana}</li>
+              <li>{word.mean}</li>
+              <li>{word.example}</li>
+              <li>{word.exampleKana}</li>
+              <li>{word.exampleMean}</li>
+            </ul>
+          ))}
+        </div>
+        <button
+          className="w-fit mt-20 p-5 border border-black"
+          onClick={() => {}}
+        >
+          db 삽입
+        </button>
+      </div>
+    </div>
+  );
 }
